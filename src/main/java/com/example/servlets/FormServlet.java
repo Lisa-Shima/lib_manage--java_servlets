653f080a945bed20597c38b64d923840adf44696
= <=>
2025-01-10 13:52:49 +0200

    Committing - Form Handling(Handling post requests)

---
+++ src/main/java/com/example/servlets/FormServlet.java
@@ -0,0 +1,18 @@
+package com.example.servlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+public class FormServlet extends HttpServlet{
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        res.setContentType("text/html");
+        res.getWriter().println("<h1>Form Submitted successfully!</h1>");
+        res.getWriter().println("<p>Name: " + name + "</p>");
+        res.getWriter().println("<p>Email: " + email + "</p>");
+    }
+}
